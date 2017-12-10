aab046eb75c08619151462e395ce3fbbba729078
x10an14 <x10an14@hotmail.com>
2017-12-10 14:16:56 +0100

    Rust mini update - not working

--- imaginary_numbers/rust/src/main.rs
+++ imaginary_numbers/rust/src/main.rs
@@ -1,3 +1,33 @@
+extern crate num;
+use num::Float;
+
+pub struct Complex<T> {
+    pub re: T,
+    pub im: T,
+}
+
+impl<T: Clone> Complex<T> {
+    fn new(re: T, im: T) -> Complex<T> {
+        unimplemented!();
+    }
+
+    fn i() -> Complex<T> {
+        unimplemented!();
+    }
+
+    fn scale(&self, factor: T) -> Complex<T> {
+        unimplemented!();
+    }
+
+    fn unscale(&self, factor: T) -> Complex<T> {
+        unimplemented!();
+    }
+
+
+
+}
+
+
 fn main() {
     println!("Hello, world!");
 }
